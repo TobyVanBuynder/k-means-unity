90edb4a76de1854c090b11340722c439471fce75
Toby Van Buynder <tobyvbuynder@gmail.com>
2024-07-25 19:37:14 +0200

    Added fade in to scene background music

---
+++ Assets/Scripts/Utilities/AudioFade.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioFade : MonoBehaviour
+{
+    [SerializeField] float _fadeTime = 0.5f;
+    [SerializeField] AnimationCurve _fadeCurve;
+
+    float _currentTimeOnCurve;
+    AudioSource _audioSource;
+
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.volume = 0f;
+        _currentTimeOnCurve = 0f;
+    }
+
+    void Update()
+    {
+        bool isDone = false;
+
+        _currentTimeOnCurve += Time.deltaTime;
+        if (_currentTimeOnCurve > _fadeTime)
+        {
+            _currentTimeOnCurve = _fadeTime;
+            isDone = true;
+        }
+        _currentTimeOnCurve = Mathf.Clamp(_currentTimeOnCurve, 0f, _fadeTime);
+
+        float fadeCurveValue = _fadeCurve.Evaluate(_currentTimeOnCurve / _fadeTime);
+        _audioSource.volume = Mathf.Clamp01(fadeCurveValue);
+
+        if (isDone)
+        {
+            enabled = false;
+        }
+    }
+}
